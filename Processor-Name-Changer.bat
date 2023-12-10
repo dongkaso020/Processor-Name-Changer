c72b40db8e7f99f7a63420cfe20025bd4c83aef1
Candidness <105561951+dongkaso020@users.noreply.github.com>
2023-12-10 15:06:48 +0800

    this is batch file

---
+++ Processor-Name-Changer.bat
@@ -0,0 +1,295 @@
+@echo off
+echo Windows Registry Editor Version 5.00 > temp.reg
+echo. >> temp.reg
+echo [HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor] >> temp.reg
+echo. >> temp.reg
+echo [HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\0] >> temp.reg
+echo "Component Information"=hex:00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00 >> temp.reg
+echo "Identifier"="Intel64 Family 6 Model 151 Stepping 1" >> temp.reg
+echo "Configuration Data"=hex(9):ff,ff,ff,ff,ff,ff,ff,ff,00,00,00,00,00,00,00,00 >> temp.reg
+echo "ProcessorNameString"="12th Gen Intel(R) Core(TM) i9-12900KF" >> temp.reg
+echo "VendorIdentifier"="GenuineIntel" >> temp.reg
+echo "FeatureSet"=dword:3d1b3fff >> temp.reg
+echo "~MHz"=dword:000004a6 >> temp.reg
+echo "Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Update Status"=dword:00000002 >> temp.reg
+echo "Previous Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Platform Specific Field 1"=dword:00000002 >> temp.reg
+echo [HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\1] >> temp.reg
+echo "Component Information"=hex:00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00 >> temp.reg
+echo "Identifier"="Intel64 Family 6 Model 151 Stepping 1" >> temp.reg
+echo "Configuration Data"=hex(9):ff,ff,ff,ff,ff,ff,ff,ff,00,00,00,00,00,00,00,00 >> temp.reg
+echo "ProcessorNameString"="12th Gen Intel(R) Core(TM) i9-12900KF" >> temp.reg
+echo "VendorIdentifier"="GenuineIntel" >> temp.reg
+echo "FeatureSet"=dword:3d1b3fff >> temp.reg
+echo "~MHz"=dword:000004a6 >> temp.reg
+echo "Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Update Status"=dword:00000002 >> temp.reg
+echo "Previous Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Platform Specific Field 1"=dword:00000002 >> temp.reg
+echo [HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\2] >> temp.reg
+echo "Component Information"=hex:00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00 >> temp.reg
+echo "Identifier"="Intel64 Family 6 Model 151 Stepping 1" >> temp.reg
+echo "Configuration Data"=hex(9):ff,ff,ff,ff,ff,ff,ff,ff,00,00,00,00,00,00,00,00 >> temp.reg
+echo "ProcessorNameString"="12th Gen Intel(R) Core(TM) i9-12900KF" >> temp.reg
+echo "VendorIdentifier"="GenuineIntel" >> temp.reg
+echo "FeatureSet"=dword:3d1b3fff >> temp.reg
+echo "~MHz"=dword:000004a6 >> temp.reg
+echo "Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Update Status"=dword:00000002 >> temp.reg
+echo "Previous Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Platform Specific Field 1"=dword:00000002 >> temp.reg
+echo [HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\3] >> temp.reg
+echo "Component Information"=hex:00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00 >> temp.reg
+echo "Identifier"="Intel64 Family 6 Model 151 Stepping 1" >> temp.reg
+echo "Configuration Data"=hex(9):ff,ff,ff,ff,ff,ff,ff,ff,00,00,00,00,00,00,00,00 >> temp.reg
+echo "ProcessorNameString"="12th Gen Intel(R) Core(TM) i9-12900KF" >> temp.reg
+echo "VendorIdentifier"="GenuineIntel" >> temp.reg
+echo "FeatureSet"=dword:3d1b3fff >> temp.reg
+echo "~MHz"=dword:000004a6 >> temp.reg
+echo "Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Update Status"=dword:00000002 >> temp.reg
+echo "Previous Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Platform Specific Field 1"=dword:00000002 >> temp.reg
+echo [HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\4] >> temp.reg
+echo "Component Information"=hex:00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00 >> temp.reg
+echo "Identifier"="Intel64 Family 6 Model 151 Stepping 1" >> temp.reg
+echo "Configuration Data"=hex(9):ff,ff,ff,ff,ff,ff,ff,ff,00,00,00,00,00,00,00,00 >> temp.reg
+echo "ProcessorNameString"="12th Gen Intel(R) Core(TM) i9-12900KF" >> temp.reg
+echo "VendorIdentifier"="GenuineIntel" >> temp.reg
+echo "FeatureSet"=dword:3d1b3fff >> temp.reg
+echo "~MHz"=dword:000004a6 >> temp.reg
+echo "Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Update Status"=dword:00000002 >> temp.reg
+echo "Previous Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Platform Specific Field 1"=dword:00000002 >> temp.reg
+echo [HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\5] >> temp.reg
+echo "Component Information"=hex:00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00 >> temp.reg
+echo "Identifier"="Intel64 Family 6 Model 151 Stepping 1" >> temp.reg
+echo "Configuration Data"=hex(9):ff,ff,ff,ff,ff,ff,ff,ff,00,00,00,00,00,00,00,00 >> temp.reg
+echo "ProcessorNameString"="12th Gen Intel(R) Core(TM) i9-12900KF" >> temp.reg
+echo "VendorIdentifier"="GenuineIntel" >> temp.reg
+echo "FeatureSet"=dword:3d1b3fff >> temp.reg
+echo "~MHz"=dword:000004a6 >> temp.reg
+echo "Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Update Status"=dword:00000002 >> temp.reg
+echo "Previous Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Platform Specific Field 1"=dword:00000002 >> temp.reg
+echo [HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\6] >> temp.reg
+echo "Component Information"=hex:00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00 >> temp.reg
+echo "Identifier"="Intel64 Family 6 Model 151 Stepping 1" >> temp.reg
+echo "Configuration Data"=hex(9):ff,ff,ff,ff,ff,ff,ff,ff,00,00,00,00,00,00,00,00 >> temp.reg
+echo "ProcessorNameString"="12th Gen Intel(R) Core(TM) i9-12900KF" >> temp.reg
+echo "VendorIdentifier"="GenuineIntel" >> temp.reg
+echo "FeatureSet"=dword:3d1b3fff >> temp.reg
+echo "~MHz"=dword:000004a6 >> temp.reg
+echo "Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Update Status"=dword:00000002 >> temp.reg
+echo "Previous Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Platform Specific Field 1"=dword:00000002 >> temp.reg
+echo [HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\7] >> temp.reg
+echo "Component Information"=hex:00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00 >> temp.reg
+echo "Identifier"="Intel64 Family 6 Model 151 Stepping 1" >> temp.reg
+echo "Configuration Data"=hex(9):ff,ff,ff,ff,ff,ff,ff,ff,00,00,00,00,00,00,00,00 >> temp.reg
+echo "ProcessorNameString"="12th Gen Intel(R) Core(TM) i9-12900KF" >> temp.reg
+echo "VendorIdentifier"="GenuineIntel" >> temp.reg
+echo "FeatureSet"=dword:3d1b3fff >> temp.reg
+echo "~MHz"=dword:000004a6 >> temp.reg
+echo "Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Update Status"=dword:00000002 >> temp.reg
+echo "Previous Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Platform Specific Field 1"=dword:00000002 >> temp.reg
+echo [HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\8] >> temp.reg
+echo "Component Information"=hex:00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00 >> temp.reg
+echo "Identifier"="Intel64 Family 6 Model 151 Stepping 1" >> temp.reg
+echo "Configuration Data"=hex(9):ff,ff,ff,ff,ff,ff,ff,ff,00,00,00,00,00,00,00,00 >> temp.reg
+echo "ProcessorNameString"="12th Gen Intel(R) Core(TM) i9-12900KF" >> temp.reg
+echo "VendorIdentifier"="GenuineIntel" >> temp.reg
+echo "FeatureSet"=dword:3d1b3fff >> temp.reg
+echo "~MHz"=dword:000004a6 >> temp.reg
+echo "Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Update Status"=dword:00000002 >> temp.reg
+echo "Previous Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Platform Specific Field 1"=dword:00000002 >> temp.reg
+echo [HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\9] >> temp.reg
+echo "Component Information"=hex:00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00 >> temp.reg
+echo "Identifier"="Intel64 Family 6 Model 151 Stepping 1" >> temp.reg
+echo "Configuration Data"=hex(9):ff,ff,ff,ff,ff,ff,ff,ff,00,00,00,00,00,00,00,00 >> temp.reg
+echo "ProcessorNameString"="12th Gen Intel(R) Core(TM) i9-12900KF" >> temp.reg
+echo "VendorIdentifier"="GenuineIntel" >> temp.reg
+echo "FeatureSet"=dword:3d1b3fff >> temp.reg
+echo "~MHz"=dword:000004a6 >> temp.reg
+echo "Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Update Status"=dword:00000002 >> temp.reg
+echo "Previous Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Platform Specific Field 1"=dword:00000002 >> temp.reg
+echo [HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\10] >> temp.reg
+echo "Component Information"=hex:00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00 >> temp.reg
+echo "Identifier"="Intel64 Family 6 Model 151 Stepping 1" >> temp.reg
+echo "Configuration Data"=hex(9):ff,ff,ff,ff,ff,ff,ff,ff,00,00,00,00,00,00,00,00 >> temp.reg
+echo "ProcessorNameString"="12th Gen Intel(R) Core(TM) i9-12900KF" >> temp.reg
+echo "VendorIdentifier"="GenuineIntel" >> temp.reg
+echo "FeatureSet"=dword:3d1b3fff >> temp.reg
+echo "~MHz"=dword:000004a6 >> temp.reg
+echo "Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Update Status"=dword:00000002 >> temp.reg
+echo "Previous Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Platform Specific Field 1"=dword:00000002 >> temp.reg
+echo [HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\11] >> temp.reg
+echo "Component Information"=hex:00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00 >> temp.reg
+echo "Identifier"="Intel64 Family 6 Model 151 Stepping 1" >> temp.reg
+echo "Configuration Data"=hex(9):ff,ff,ff,ff,ff,ff,ff,ff,00,00,00,00,00,00,00,00 >> temp.reg
+echo "ProcessorNameString"="12th Gen Intel(R) Core(TM) i9-12900KF" >> temp.reg
+echo "VendorIdentifier"="GenuineIntel" >> temp.reg
+echo "FeatureSet"=dword:3d1b3fff >> temp.reg
+echo "~MHz"=dword:000004a6 >> temp.reg
+echo "Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Update Status"=dword:00000002 >> temp.reg
+echo "Previous Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Platform Specific Field 1"=dword:00000002 >> temp.reg
+echo [HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\12] >> temp.reg
+echo "Component Information"=hex:00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00 >> temp.reg
+echo "Identifier"="Intel64 Family 6 Model 151 Stepping 1" >> temp.reg
+echo "Configuration Data"=hex(9):ff,ff,ff,ff,ff,ff,ff,ff,00,00,00,00,00,00,00,00 >> temp.reg
+echo "ProcessorNameString"="12th Gen Intel(R) Core(TM) i9-12900KF" >> temp.reg
+echo "VendorIdentifier"="GenuineIntel" >> temp.reg
+echo "FeatureSet"=dword:3d1b3fff >> temp.reg
+echo "~MHz"=dword:000004a6 >> temp.reg
+echo "Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Update Status"=dword:00000002 >> temp.reg
+echo "Previous Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Platform Specific Field 1"=dword:00000002 >> temp.reg
+echo [HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\13] >> temp.reg
+echo "Component Information"=hex:00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00 >> temp.reg
+echo "Identifier"="Intel64 Family 6 Model 151 Stepping 1" >> temp.reg
+echo "Configuration Data"=hex(9):ff,ff,ff,ff,ff,ff,ff,ff,00,00,00,00,00,00,00,00 >> temp.reg
+echo "ProcessorNameString"="12th Gen Intel(R) Core(TM) i9-12900KF" >> temp.reg
+echo "VendorIdentifier"="GenuineIntel" >> temp.reg
+echo "FeatureSet"=dword:3d1b3fff >> temp.reg
+echo "~MHz"=dword:000004a6 >> temp.reg
+echo "Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Update Status"=dword:00000002 >> temp.reg
+echo "Previous Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Platform Specific Field 1"=dword:00000002 >> temp.reg
+echo [HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\14] >> temp.reg
+echo "Component Information"=hex:00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00 >> temp.reg
+echo "Identifier"="Intel64 Family 6 Model 151 Stepping 1" >> temp.reg
+echo "Configuration Data"=hex(9):ff,ff,ff,ff,ff,ff,ff,ff,00,00,00,00,00,00,00,00 >> temp.reg
+echo "ProcessorNameString"="12th Gen Intel(R) Core(TM) i9-12900KF" >> temp.reg
+echo "VendorIdentifier"="GenuineIntel" >> temp.reg
+echo "FeatureSet"=dword:3d1b3fff >> temp.reg
+echo "~MHz"=dword:000004a6 >> temp.reg
+echo "Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Update Status"=dword:00000002 >> temp.reg
+echo "Previous Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Platform Specific Field 1"=dword:00000002 >> temp.reg
+echo [HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\15] >> temp.reg
+echo "Component Information"=hex:00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00 >> temp.reg
+echo "Identifier"="Intel64 Family 6 Model 151 Stepping 1" >> temp.reg
+echo "Configuration Data"=hex(9):ff,ff,ff,ff,ff,ff,ff,ff,00,00,00,00,00,00,00,00 >> temp.reg
+echo "ProcessorNameString"="12th Gen Intel(R) Core(TM) i9-12900KF" >> temp.reg
+echo "VendorIdentifier"="GenuineIntel" >> temp.reg
+echo "FeatureSet"=dword:3d1b3fff >> temp.reg
+echo "~MHz"=dword:000004a6 >> temp.reg
+echo "Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Update Status"=dword:00000002 >> temp.reg
+echo "Previous Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Platform Specific Field 1"=dword:00000002 >> temp.reg
+echo [HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\2] >> temp.reg
+echo "Component Information"=hex:00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00 >> temp.reg
+echo "Identifier"="Intel64 Family 6 Model 151 Stepping 1" >> temp.reg
+echo "Configuration Data"=hex(9):ff,ff,ff,ff,ff,ff,ff,ff,00,00,00,00,00,00,00,00 >> temp.reg
+echo "ProcessorNameString"="12th Gen Intel(R) Core(TM) i9-12900KF" >> temp.reg
+echo "VendorIdentifier"="GenuineIntel" >> temp.reg
+echo "FeatureSet"=dword:3d1b3fff >> temp.reg
+echo "~MHz"=dword:000004a6 >> temp.reg
+echo "Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Update Status"=dword:00000002 >> temp.reg
+echo "Previous Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Platform Specific Field 1"=dword:00000002 >> temp.reg
+echo [HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\3] >> temp.reg
+echo "Component Information"=hex:00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00 >> temp.reg
+echo "Identifier"="Intel64 Family 6 Model 151 Stepping 1" >> temp.reg
+echo "Configuration Data"=hex(9):ff,ff,ff,ff,ff,ff,ff,ff,00,00,00,00,00,00,00,00 >> temp.reg
+echo "ProcessorNameString"="12th Gen Intel(R) Core(TM) i9-12900KF" >> temp.reg
+echo "VendorIdentifier"="GenuineIntel" >> temp.reg
+echo "FeatureSet"=dword:3d1b3fff >> temp.reg
+echo "~MHz"=dword:000004a6 >> temp.reg
+echo "Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Update Status"=dword:00000002 >> temp.reg
+echo "Previous Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Platform Specific Field 1"=dword:00000002 >> temp.reg
+echo [HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\4] >> temp.reg
+echo "Component Information"=hex:00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00 >> temp.reg
+echo "Identifier"="Intel64 Family 6 Model 151 Stepping 1" >> temp.reg
+echo "Configuration Data"=hex(9):ff,ff,ff,ff,ff,ff,ff,ff,00,00,00,00,00,00,00,00 >> temp.reg
+echo "ProcessorNameString"="12th Gen Intel(R) Core(TM) i9-12900KF" >> temp.reg
+echo "VendorIdentifier"="GenuineIntel" >> temp.reg
+echo "FeatureSet"=dword:3d1b3fff >> temp.reg
+echo "~MHz"=dword:000004a6 >> temp.reg
+echo "Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Update Status"=dword:00000002 >> temp.reg
+echo "Previous Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Platform Specific Field 1"=dword:00000002 >> temp.reg
+echo [HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\5] >> temp.reg
+echo "Component Information"=hex:00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00 >> temp.reg
+echo "Identifier"="Intel64 Family 6 Model 151 Stepping 1" >> temp.reg
+echo "Configuration Data"=hex(9):ff,ff,ff,ff,ff,ff,ff,ff,00,00,00,00,00,00,00,00 >> temp.reg
+echo "ProcessorNameString"="12th Gen Intel(R) Core(TM) i9-12900KF" >> temp.reg
+echo "VendorIdentifier"="GenuineIntel" >> temp.reg
+echo "FeatureSet"=dword:3d1b3fff >> temp.reg
+echo "~MHz"=dword:000004a6 >> temp.reg
+echo "Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Update Status"=dword:00000002 >> temp.reg
+echo "Previous Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Platform Specific Field 1"=dword:00000002 >> temp.reg
+echo [HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\6] >> temp.reg
+echo "Component Information"=hex:00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00 >> temp.reg
+echo "Identifier"="Intel64 Family 6 Model 151 Stepping 1" >> temp.reg
+echo "Configuration Data"=hex(9):ff,ff,ff,ff,ff,ff,ff,ff,00,00,00,00,00,00,00,00 >> temp.reg
+echo "ProcessorNameString"="12th Gen Intel(R) Core(TM) i9-12900KF" >> temp.reg
+echo "VendorIdentifier"="GenuineIntel" >> temp.reg
+echo "FeatureSet"=dword:3d1b3fff >> temp.reg
+echo "~MHz"=dword:000004a6 >> temp.reg
+echo "Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Update Status"=dword:00000002 >> temp.reg
+echo "Previous Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Platform Specific Field 1"=dword:00000002 >> temp.reg
+echo [HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\7] >> temp.reg
+echo "Component Information"=hex:00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00 >> temp.reg
+echo "Identifier"="Intel64 Family 6 Model 151 Stepping 1" >> temp.reg
+echo "Configuration Data"=hex(9):ff,ff,ff,ff,ff,ff,ff,ff,00,00,00,00,00,00,00,00 >> temp.reg
+echo "ProcessorNameString"="12th Gen Intel(R) Core(TM) i9-12900KF" >> temp.reg
+echo "VendorIdentifier"="GenuineIntel" >> temp.reg
+echo "FeatureSet"=dword:3d1b3fff >> temp.reg
+echo "~MHz"=dword:000004a6 >> temp.reg
+echo "Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Update Status"=dword:00000002 >> temp.reg
+echo "Previous Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Platform Specific Field 1"=dword:00000002 >> temp.reg
+echo [HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\8] >> temp.reg
+echo "Component Information"=hex:00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00 >> temp.reg
+echo "Identifier"="Intel64 Family 6 Model 151 Stepping 1" >> temp.reg
+echo "Configuration Data"=hex(9):ff,ff,ff,ff,ff,ff,ff,ff,00,00,00,00,00,00,00,00 >> temp.reg
+echo "ProcessorNameString"="12th Gen Intel(R) Core(TM) i9-12900KF" >> temp.reg
+echo "VendorIdentifier"="GenuineIntel" >> temp.reg
+echo "FeatureSet"=dword:3d1b3fff >> temp.reg
+echo "~MHz"=dword:000004a6 >> temp.reg
+echo "Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Update Status"=dword:00000002 >> temp.reg
+echo "Previous Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Platform Specific Field 1"=dword:00000002 >> temp.reg
+echo [HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\9] >> temp.reg
+echo "Component Information"=hex:00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00 >> temp.reg
+echo "Identifier"="Intel64 Family 6 Model 151 Stepping 1" >> temp.reg
+echo "Configuration Data"=hex(9):ff,ff,ff,ff,ff,ff,ff,ff,00,00,00,00,00,00,00,00 >> temp.reg
+echo "ProcessorNameString"="12th Gen Intel(R) Core(TM) i9-12900KF" >> temp.reg
+echo "VendorIdentifier"="GenuineIntel" >> temp.reg
+echo "FeatureSet"=dword:3d1b3fff >> temp.reg
+echo "~MHz"=dword:000004a6 >> temp.reg
+echo "Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Update Status"=dword:00000002 >> temp.reg
+echo "Previous Update Revision"=hex:00,00,00,00,1a,00,00,00 >> temp.reg
+echo "Platform Specific Field 1"=dword:00000002 >> temp.reg
+regedit /s temp.reg
+del /f temp.reg
